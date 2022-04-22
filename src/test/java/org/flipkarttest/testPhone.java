71041d8123eea3d000220bfefa9138cb13883194
KarthicknathChowdhary <karthicknathchowdhary@gmail.com>
2022-04-22 23:02:22 +0530

    flipkart project

---
+++ src/test/java/org/flipkarttest/testPhone.java
@@ -0,0 +1,27 @@
+package org.flipkarttest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class testPhone {
+	
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.flipkart.com/");
+		
+		driver.manage().window().maximize();
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.quit();
+		
+	}
+
+}
